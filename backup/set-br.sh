efa326332a2ecb60082d55e2fa181ca5adfc3f63
Mahboub-power-is-back <vpsplus089@gmail.com>
2025-10-14 09:02:18 +0100

    Add files via upload

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/senowahyu62/scriptvps/main/backup"
+akbarvpn="raw.githubusercontent.com/Mahboub-power-is-back/update244/refs/heads/main/backup"
 
 apt install rclone -y
 printf "q\n" | rclone config
