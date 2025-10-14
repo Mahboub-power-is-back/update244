ef785792f840ff06324f7985512c0f56a7924845
Mahboub-power-is-back <vpsplus089@gmail.com>
2025-10-14 13:40:01 +0100

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -201,25 +201,19 @@ rm -f /etc/default/sslh
 
 # Settings SSLH
 cat > /etc/default/sslh <<-END
-# Default options for sslh initscript
-# sourced by /etc/init.d/sslh
+[Unit]
+Description=SSL/SSH multiplexer
+After=network.target
 
-# Disabled by default, to force yourself
-# to read the configuration:
-# - /usr/share/doc/sslh/README.Debian (quick start)
-# - /usr/share/doc/sslh/README, at "Configuration" section
-# - sslh(8) via "man sslh" for more configuration details.
-# Once configuration ready, you *must* set RUN to yes here
-# and try to start sslh (standalone mode only)
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/sbin/sslh --user sslh --listen 0.0.0.0:443 --tls 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 -n -f
+Restart=on-failure
+RestartSec=3
 
-RUN=yes
-
-# binary to use: forked (sslh) or single-thread (sslh-select) version
-# systemd users: don't forget to modify /lib/systemd/system/sslh.service
-DAEMON=/usr/sbin/sslh
-
-DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --tls 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh.pid -n"
-
+[Install]
+WantedBy=multi-user.target
 END
 
 # Restart Service SSLH
