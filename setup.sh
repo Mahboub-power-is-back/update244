98b93cd8900db35573af054364e5bf2aac32fe4b
Mahboub-power-is-back <vpsplus089@gmail.com>
2025-10-14 09:10:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -125,7 +125,7 @@ echo "   - XRAYS Vmess TLS         : 8443"  | tee -a log-install.txt
 echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - XRAYS Vless TLS         : 8443"  | tee -a log-install.txt
 echo "   - XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
-echo "   - XRAYS Trojan            : 2083"  | tee -a log-install.txt
+echo "   - XRAYS Trojan            : 8443"  | tee -a log-install.txt
 echo "   - Websocket TLS           : 443"  | tee -a log-install.txt
 echo "   - Websocket None TLS      : 8880"  | tee -a log-install.txt
 echo "   - Websocket Ovpn          : 2086"  | tee -a log-install.txt
