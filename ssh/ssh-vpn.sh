6c3fde029d94afc5de94e7853f66075609aef559
Mahboub-power-is-back <vpsplus089@gmail.com>
2025-10-15 12:25:59 +0100

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -199,28 +199,22 @@ apt -y install sslh
 rm -f /etc/default/sslh
 
 # Settings SSLH
-cat > /etc/default/sslh <<-END
-# Default options for sslh initscript
-# sourced by /etc/init.d/sslh
+rm -f /etc/default/sslh
 
-# Disabled by default, to force yourself
-# to read the configuration:
-# - /usr/share/doc/sslh/README.Debian (quick start)
-# - /usr/share/doc/sslh/README, at "Configuration" section
-# - sslh(8) via "man sslh" for more configuration details.
-# Once configuration ready, you *must* set RUN to yes here
-# and try to start sslh (standalone mode only)
-
+cat > /etc/default/sslh << 'EOF'
 RUN=yes
-
-# binary to use: forked (sslh) or single-thread (sslh-select) version
-# systemd users: don't forget to modify /lib/systemd/system/sslh.service
 DAEMON=/usr/sbin/sslh
 
-DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --tls 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 -n -f
-Restart=on-failure"
-END
-
+DAEMON_OPTS="--user sslh \
+  --listen 0.0.0.0:443 \
+  --tls 127.0.0.1:777 \
+  --ssh 127.0.0.1:109 \
+  --openvpn 127.0.0.1:1194 \
+  --http 127.0.0.1:8880 \
+  -n"
+EOF
+sed -i 's|ExecStart=.*|ExecStart=/usr/sbin/sslh -f $DAEMON_OPTS|' /lib/systemd/system/sslh.service
+sed -i '/^\[Service\]/a Restart=on-failure' /lib/systemd/system/sslh.service
 # Restart Service SSLH
 service sslh restart
 systemctl restart sslh
