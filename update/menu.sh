4aafdc8032368dcaaccf0c8ae658bf5a83178948
Mahboub-power-is-back <vpsplus089@gmail.com>
2025-10-26 00:31:57 +0100

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 clear
-
 # Color definitions
 m="\033[0;1;36m"  # Cyan
 y="\033[0;1;37m"  # White
 yy="\033[0;1;32m" # Green
 yl="\033[0;1;33m" # Yellow
 wh="\033[0m"      # Reset
-
 # Function to display centered text
 center_text() {
     local text="$1"
@@ -16,9 +14,41 @@ center_text() {
     printf "%${padding}s" ''
     echo -e "$text"
 }
-
+# Function to get system information
+get_system_info() {
+    # Get OS information
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        OS=$PRETTY_NAME
+    else
+        OS=$(uname -s)
+    fi
+    # Get domain from /etc/xray/domain if available
+    if [ -f /etc/xray/domain ]; then
+        DOMAIN=$(cat /etc/xray/domain)
+    else
+        DOMAIN=$(hostname -f 2>/dev/null || hostname)
+    fi
+    # Get current time
+    TIME=$(date '+%Y-%m-%d %H:%M:%S')
+    # Get online users (SSH connections)
+    ONLINE_USERS=$(who | cut -d' ' -f1 | sort -u | wc -l)
+}
+# Get system information
+get_system_info
+# Display header with system info
 echo -e "$y$(center_text '┌──────────────────────────────────────────┐')$wh"
-echo -e "$y$(center_text '│             MENU UTAMA                   │')$wh"
+echo -e "$y$(center_text '│              SYSTEM INFO                 │')$wh"
+echo -e "$y$(center_text '├──────────────────────────────────────────┤')$wh"
+echo -e "$y   │$yy OS:    $yl$OS$y"
+echo -e "$y   │$yy Domain:$yl $DOMAIN$y"
+echo -e "$y   │$yy Time:  $yl $TIME$y"
+echo -e "$y   │$yy Users: $yl $ONLINE_USERS Connected$y"
+echo -e "$y$(center_text '└──────────────────────────────────────────┘')$wh"
+echo
+# Main Menu
+echo -e "$y$(center_text '┌──────────────────────────────────────────┐')$wh"
+echo -e "$y$(center_text '│             MAIN MENU                    │')$wh"
 echo -e "$y$(center_text '└──────────────────────────────────────────┘')$wh"
 echo
 echo -e "$y  ┌───┬────────────────────────────────────────┐$wh"
@@ -42,8 +72,7 @@ echo -e "$y$(center_text '│   Select From Options [ 1 - 13 ]         │')$wh"
 echo -e "$y$(center_text '└──────────────────────────────────────────┘')$wh"
 echo
 read -p "$(echo -e "$y│ $whSelect option: $y")" menu
-
-case \$menu in
+case $menu in
 1)
 clear
 sshovpnmenu
@@ -98,7 +127,7 @@ exit
 ;;
 *)
 clear
-echo -e "\$yl Invalid selection. Please try again.\$wh"
+echo -e "$yl Invalid selection. Please try again.$wh"
 sleep 2
 menu
 ;;
