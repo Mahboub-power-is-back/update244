ea9b23730fb61a6a44731bc626b123b5f60eeae2
Mahboub-power-is-back <vpsplus089@gmail.com>
2025-10-26 12:39:05 +0100

    Update setmenu.sh

--- update/setmenu.sh
+++ update/setmenu.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
-#!/bin/bash
-clear
-m="\033[0;1;36m"
-y="\033[0;1;37m"
-yy="\033[0;1;32m"
-yl="\033[0;1;33m"
-wh="\033[0m"
-echo -e "$y                   SYSTEM SETTING$wh"
-echo -e "$y-------------------------------------------------------------$wh"
-echo -e "$yy 1$y.  Add Or Change Subdomain Host For VPS"
-echo -e "$yy 2$y.  Change Port Of Some Service"
-echo -e "$yy 3$y.  Autobackup Data VPS"
-echo -e "$yy 4$y.  Backup Data VPS"
-echo -e "$yy 5$y.  Restore Data VPS"
-echo -e "$yy 6$y.  Webmin Menu"
-echo -e "$yy 7$y.  Limit Bandwith Speed Server"
-echo -e "$yy 8$y.  Check Usage of VPS Ram"
-echo -e "$yy 9$y.  Reboot VPS"
-echo -e "$yy 10$y. Speedtest VPS"
-echo -e "$yy 11$y. Displaying System Information"
-echo -e "$yy 12$y. Info Script Auto Install"
-echo -e "$yy 13$y. Menu"
-echo -e "$yy 14$y. Exit"
-echo -e "$y-------------------------------------------------------------$wh"
-echo -e ""
-read -p "Select From Options [ 1 - 60 ] : " menu
-echo -e ""
+# Color definitions
+m="\033[0;1;36m"  # Cyan
+y="\033[0;1;37m"  # White
+yy="\033[0;1;32m" # Green
+yl="\033[0;1;33m" # Yellow
+wh="\033[0m"      # Reset
+# Function to display centered text
+center_text() {
+    local text="$1"
+    local width=50
+    local padding=$(( (width - ${#text}) / 2 ))
+    printf "%${padding}s" ''
+    echo -e "$text"
+}
+echo -e "$y┌─────────────────────────────────────────────────────────────────┐$wh"
+echo -e "$y│$m                       SYSTEM SETTINGS$y                           │$wh"
+echo -e "$y├─────────────────────────────────────────────────────────────────┤$wh"
+echo -e "$y│  $yy[01]$y ─ Add Or Change Subdomain Host For VPS$y                    │$wh"
+echo -e "$y│  $yy[02]$y ─ Change Port Of Some Service$y                             │$wh"
+echo -e "$y│  $yy[03]$y ─ Autobackup Data VPS$y                                     │$wh"
+echo -e "$y│  $yy[04]$y ─ Backup Data VPS$y                                         │$wh"
+echo -e "$y│  $yy[05]$y ─ Restore Data VPS$y                                        │$wh"
+echo -e "$y│  $yy[06]$y ─ Webmin Menu$y                                             │$wh"
+echo -e "$y│  $yy[07]$y ─ Limit Bandwidth Speed Server$y                            │$wh"
+echo -e "$y│  $yy[08]$y ─ Check Usage of VPS Ram$y                                  │$wh"
+echo -e "$y│  $yy[09]$y ─ Reboot VPS$y                                              │$wh"
+echo -e "$y│  $yy[10]$y ─ Speedtest VPS$y                                           │$wh"
+echo -e "$y│  $yy[11]$y ─ Displaying System Information$y                           │$wh"
+echo -e "$y│  $yy[12]$y ─ Info Script Auto Install$y                                │$wh"
+echo -e "$y│  $yy[13]$y ─ Main Menu$y                                               │$wh"
+echo -e "$y│  $yy[14]$y ─ Exit$y                                                    │$wh"
+echo -e "$y├─────────────────────────────────────────────────────────────────┤$wh"
+echo -e "$y│$m                   Autoscript By : mahboub-million$y               │$wh"
+echo -e "$y└─────────────────────────────────────────────────────────────────┘$wh"
+echo
+echo -e "$y$(center_text '┌──────────────────────────────────────────┐')$wh"
+echo -e "$y$(center_text '│   Select From Options [ 1 - 14 ]         │')$wh"
+echo -e "$y$(center_text '└──────────────────────────────────────────┘')$wh"
+echo
+read -p "$(echo -e "$y│ $whSelect option: $y")" menu
 case $menu in
 1)
 addhost
@@ -73,6 +86,8 @@ exit
 ;;
 *)
 clear
-menu
+echo -e "$yl❌  Invalid selection! Please try again.$wh"
+sleep 2
+setmenu
 ;;
 esac
