fd25d9a182090f915382f2578864a2d6d5374808
Mahboub-power-is-back <vpsplus089@gmail.com>
2025-10-26 01:21:16 +0100

    Update wgmenu.sh

--- update/wgmenu.sh
+++ update/wgmenu.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 clear
+
+# Color definitions
 m="\033[0;1;36m"
 y="\033[0;1;37m"
 yy="\033[0;1;32m"
 yl="\033[0;1;33m"
 wh="\033[0m"
-echo -e "$y                          WIREGUARD $wh"
-echo -e "$y-------------------------------------------------------------$wh"
-echo -e "$yy 1$y. Create Account Wireguard"
-echo -e "$yy 2$y. Delete Account Wireguard"
-echo -e "$yy 3$y. Extending Account Wireguard Active Life"
-echo -e "$yy 4$y. Menu"
-echo -e "$yy 5$y. Exit"
-echo -e "$y-------------------------------------------------------------$wh"
-read -p "Select From Options [ 1 - 5 ] : " menu
+
+# Function to display centered text
+center_text() {
+    local text="$1"
+    local width=50
+    local padding=$(( (width - ${#text}) / 2 ))
+    printf "%${padding}s" ''
+    echo -e "$text"
+}
+
+echo -e "$y$(center_text '┌──────────────────────────────────────────┐')$wh"
+echo -e "$y$(center_text '│              WIREGUARD MENU              │')$wh"
+echo -e "$y$(center_text '└──────────────────────────────────────────┘')$wh"
+echo
+echo -e "$y  ┌───┬────────────────────────────────────────┐$wh"
+echo -e "$y  │$yy 1 $y│  Create Account Wireguard             $y │$wh"
+echo -e "$y  │$yy 2 $y│  Delete Account Wireguard             $y │$wh"
+echo -e "$y  │$yy 3 $y│  Extend Account Active Life           $y │$wh"
+echo -e "$y  │$yy 4 $y│  Menu Utama                           $y │$wh"
+echo -e "$y  │$yy 5 $y│  Exit                                 $y │$wh"
+echo -e "$y  └───┴────────────────────────────────────────┘$wh"
+echo
+echo -e "$y$(center_text '┌──────────────────────────────────────────┐')$wh"
+echo -e "$y$(center_text '│     Select From Options [ 1 - 5 ]        │')$wh"
+echo -e "$y$(center_text '└──────────────────────────────────────────┘')$wh"
+echo
+read -p "$(echo -e "$y│ $whSelect option: $y")" menu
 echo -e ""
+
 case $menu in
 1)
 addwg
